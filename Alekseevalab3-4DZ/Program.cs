67b606a1e6c54bb8873aa2b628afec8da47343b7
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-05 16:57:07 +0300

    3.1

--- Alekseevalab3-4DZ/Program.cs
+++ Alekseevalab3-4DZ/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        enum TrafficLight { Red, Yellow, Green }
         static void Main(string[] args)
         {
-           
+            //lab3
+            //1
+            TrafficLight current = TrafficLight.Green;
+            TrafficLight next = TrafficLight.Red;
+
+            Console.WriteLine($"После {current} будет {next}");
         }
     }
 }
