636b8ea8bad21e3a43ad9d1f5ebaf23710b3add3
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-05 17:13:54 +0300

    2

--- Alekseevalab3-4DZ/Program.cs
+++ Alekseevalab3-4DZ/Program.cs
@@ -2,15 +2,30 @@
 {
     internal class Program
     {
-        enum TrafficLight { Red, Yellow, Green }
+        //enum TrafficLight { Red, Yellow, Green }
+        //enum OrderStatus { New, Processing, Shipped, Delivered }
+
         static void Main(string[] args)
         {
             //lab3
             //1
-            TrafficLight current = TrafficLight.Green;
-            TrafficLight next = TrafficLight.Red;
+            //TrafficLight current = TrafficLight.Green;
+            //TrafficLight next = TrafficLight.Red;
 
-            Console.WriteLine($"После {current} будет {next}");
+            //Console.WriteLine($"После {current} будет {next}");
+            //2
+            //CheckCancel(OrderStatus.New);
+            //CheckCancel(OrderStatus.Processing);
+            //CheckCancel(OrderStatus.Shipped);
+            //CheckCancel(OrderStatus.Delivered);
+
         }
+        //static void CheckCancel(OrderStatus status)
+        //{
+        //    if (status == OrderStatus.New || status == OrderStatus.Processing)
+        //        Console.WriteLine($"Заказ со статусом {status} можно отменить");
+        //    else
+        //        Console.WriteLine($"Заказ со статусом {status} нельзя отменить");
+        //}
     }
 }
