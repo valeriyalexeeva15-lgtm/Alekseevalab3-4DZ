b9e2aca3c84ca96d6e97ba488eae1edc6f47c675
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-05 17:48:29 +0300

    6 задание

--- Alekseevalab3-4DZ/Program.cs
+++ Alekseevalab3-4DZ/Program.cs
@@ -25,8 +25,14 @@
             //for (int i = 1; i <= 10; i++)
             //    Console.WriteLine($"5 * {i} = {5 * i}");
             //5
-            for (int i = 0; i < 10; i++)
-                Console.WriteLine("Запомни: всего одна ошибка и ты ошибся.");
+            //for (int i = 0; i < 10; i++)
+            //    Console.WriteLine("Запомни: всего одна ошибка и ты ошибся.");
+            //6
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+                Console.WriteLine($"Квадрат числа {i} равен {i * i}");
+
         }
         //static void CheckCancel(OrderStatus status)
         //{
